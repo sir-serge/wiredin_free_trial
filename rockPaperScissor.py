22f27e6a00cdd6d5bbd22fa740ea7a9884f1f55b
sir-serge <irkzsrg@gmail.com>
2025-06-26 17:33:38 +0200

    exercise 1.3 : functionlizing each funtionalty alone in its function

--- rockPaperScissor.py
+++ rockPaperScissor.py
@@ -1,10 +1,9 @@
 import random
 
-print('Start \'rock-paper-scissors\' game')
-
-print('Input your hand choice')
-your_hand = int(input('0:rock, 1:scissors, 2:paper'))
-computer_hand = random.randint(0, 2)
+# print('Start \'rock-paper-scissors\' game')
+# print('Input your hand choice')
+# your_hand = int(input('0:rock, 1:scissors, 2:paper'))
+# computer_hand = random.randint(0, 2)
 
 
 #exercise 1
@@ -34,10 +33,46 @@ computer_hand = random.randint(0, 2)
 
 #exercise 2
 
-differnece = your_hand - computer_hand
-if differnece == 0:
-    print('Draw')
-elif differnece <0:
-    print('Win')
-elif differnece > 0:
-    print('Lose')
+# differnece = your_hand - computer_hand
+# if differnece == 0:
+#     print('Draw')
+# elif differnece <0:
+#     print('Win')
+# elif differnece > 0:
+#     print('Lose')
+
+#exercise 3\
+
+#funtionalzing 
+
+def start_game():
+    print('Start \'rock-paper-scissors\' game')
+
+# get_player() function
+def get_player():
+    print('Input your hand choice')
+    your_hand = int(input('0:rock, 1:scissors, 2:paper'))
+    print('Your hand is: ', your_hand)
+    return your_hand
+
+
+#get_computer() function
+def get_computer():
+    computer_hand = random.randint(0, 2)
+    print('Computer hand is: ', computer_hand)
+    return computer_hand
+
+#result function
+def result(hand_diff):
+    if hand_diff == 0:
+        print('Draw')
+    elif hand_diff < 0:
+        print('Win')
+    elif hand_diff > 0:
+        print('Lose')
+
+start_game()
+your_hand = get_player()
+computer_hand = get_computer()
+hand_diff = your_hand - computer_hand
+result(hand_diff)
