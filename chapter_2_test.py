f650aa0df45d153d93e2635f548db7f2787b6fd2
sir-serge <irkzsrg@gmail.com>
2025-07-14 13:30:42 +0200

    chapter 2test

--- chapter_2_test.py
+++ chapter_2_test.py
@@ -1,4 +1,8 @@
 array = [
     ['January', 'February', 'March', 'April'],
-    ['May', 'June','July', 'August'], ['September', 'October', 'November', 'December']
+    ['May', 'June','July', 'August'],
+    ['September', 'October', 'November', 'December']
     ]
+for row in array:  # Loop through each sublist (each group of months)
+    for column in row:  # Loop through each month in the current sublist
+        print(column)  # Print the current month
