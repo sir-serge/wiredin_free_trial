8889a358bc88720f1223102a4457e5cd5477b556
sir-serge <irkzsrg@gmail.com>
2025-07-29 17:15:01 +0200

    creating win and lossing method

--- casino_game_chapter_4.py
+++ casino_game_chapter_4.py
@@ -1,6 +1,7 @@
 import random
 players=[]
 table=[]
+cells=[]
 
 class Player:
   def __init__(self, name,coin):
@@ -13,25 +14,33 @@ class Player:
   def info(self):
     print(f" {self.name} : {self.coin}")
 
-  def set_bet_coin(self,bet_coin):
+  def set_bet_coin(self,bet_coin,bet_cell):
      self.coin-=bet_coin
-     print(self.name + ' bets ' + str(bet_coin) + ' coin.')
+     self.bets[bet_cell] += bet_coin
+     print(self.name + ' bets ' + str(bet_coin) + ' coin(s) to '+bet_cell)
+
 
      
 
-
 class Human(Player):
-  def __init__(self, name, coin):
+  def __init__(self, name, coin,):
     super().__init__(name, coin) 
+    # self.bet_cell=bet_cell
+
 
   def bet(self):
     bet_message="How many coins do you bet: (1-99)?"
-    bet_coin=input(bet_message)
+    bet_coin=input(bet_message).strip()
+    bet_cell_message=("On what do you bet?: (R, B, 1-8)") 
+    bet_cell=input(bet_cell_message) 
+
     while not self.enable_bet_coin(bet_coin):
         bet_coin=input(bet_message)
-    super().set_bet_coin(int(bet_coin))
-    # print(bet_coin)
-    return bet_coin
+    bet_coin_int=int(bet_coin)
+
+    while not self.enable_bet_cell(bet_cell):
+        bet_cell=input(bet_cell_message) 
+    super().set_bet_coin(bet_coin_int,bet_cell)
 
   def enable_bet_coin(self,string):
     
@@ -44,6 +53,14 @@ class Human(Player):
   
         else:
             return True
+  def enable_bet_cell(self,string):
+     if string=="R" or string=="b":
+      return True
+     elif string.isdigit() and 1 <= int(string) <= 8:
+        return True
+     else:
+        return False
+        
 
 class computer(Player):
 
@@ -51,12 +68,19 @@ class computer(Player):
      super().__init__(name, coin)
 
   def bet(self):
-    coins=random.randint(1,99)
+    # coins=random.randint(1,99)
     if self.coin<=99:
-      coins=self.coin
+      coins=99
+    else:
+       coins=self.coin
     bet_coin=random.randint(1,coins)
-    super().set_bet_coin(bet_coin)
-
+    cells=[]
+    for i in table:
+      cells.append(i.__dict__['name'])
+      # print(cells)
+    set_random_key=random.randint(0,(len(cells)-1))
+    bet_cell=cells[set_random_key]
+    super().set_bet_coin(bet_coin,bet_cell)
 
 class cell(Player):
    def __init__(self, name, rate,color):
@@ -81,12 +105,16 @@ def create_player():
 
 def show_player():
    global players
-   for i in players:
-      i.info()
+  #  for i in players:
+  #     i.info()
    for i in players:
       i.bet()
+  #  for i in players:
+  #     i.info()
+
+def bet_player():
    for i in players:
-      i.info()
+       i.bet()
 
 def create_table():
     global table
@@ -112,34 +140,30 @@ def green_bar():
       return color("green","|")
 
 def show_table():
-   global table
-   header = (
-    green_bar() + "_____" +
-    green_bar()+" " + players[0].name +" "+
-    green_bar()+" " + players[2].name +" "+
-    green_bar()+" " + players[2].name +" "+
-    green_bar()+" " + players[3].name +" "+
-    green_bar()
-)
-   zeros=''
-   for i in range(len(table)+1):
-    if i==0:
-       print(header)
-    else:
-      print(green_bar() + color(table[i-1].color, ( table[i-1].name + '(x' + str(table[i-1].rate) + ')')) +green_bar()   +
-    " " +zeros.zfill(2)  +" "+
-    green_bar()+" " + zeros.zfill(2) +" "+
-    green_bar()+" " + zeros.zfill(2) +" "+
-    green_bar()+" " + zeros.zfill(2) +" "+
-    green_bar())
+  row = green_bar() + '_____' + green_bar()
+  for player in players:
+    row += player.name + green_bar()
+  print(row)
+ 
+  for cell in table:
+    row = green_bar() + color(cell.color, cell.name + '(x' + str(cell.rate) + ')') + green_bar()
+    for player in players:
+      row += str(player.bets[cell.name]).zfill(2) + green_bar()
+    print(row)
 
+def set_cell():
+   global cell
+   cells=[]
+   for i in table:
+      cells.append(cell.__dict__['name'])
 
 def play():
   print('Debug:play()')
-  create_player()
-  # show_player()
   create_table()
-  show_table()    
-
-
+  create_player()
+  
+  # show_player()
+  show_table() 
+  bet_player()
+  show_table()
 play()
