92ab6d2b3b3cbb25539db8dcf5b27468cae20add
sir-serge <irkzsrg@gmail.com>
2025-08-02 20:14:28 +0200

    using beautifullsoup

--- 5_chapter.py
+++ 5_chapter.py
@@ -4,8 +4,14 @@ from bs4 import BeautifulSoup
 url = 'https://bbc.com/news/world/'
 
 response = requests.get(url)
+
+#  Pass the HTML content, not the response object
 soup = BeautifulSoup(response.text, 'lxml')
 
-titles = soup.find_all('h2', class_='sc-4fedabc7-3')
+# Find all h3 tags with the specified class
+h2 = soup.find_all('li', class_='sc-1bad1f0d-2')
 
-print(titles)
+# print(f"{h2} h3 text")
+# # print(soup.prettify())
+
+heading=title.getText()
